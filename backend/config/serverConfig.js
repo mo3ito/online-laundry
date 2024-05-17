df149f894423c9ea6ee14eed6042c5c690add3c2
mo3ito <mostafa.tar90@gmail.com>
2024-05-17 11:05:05 +0330

    add database and connected to database

--- backend/config/serverConfig.js
+++ backend/config/serverConfig.js
@@ -1,11 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("../middlewares/corsConfig");
+const connectToDatabase = require("./database")
 
 const configureServer = (app) => {
-    app.use(express.json());
-    app.use(bodyParser.json());
-    app.use(cors);
+  app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(cors);
+
+  connectToDatabase()
 };
 
 module.exports = { configureServer };
