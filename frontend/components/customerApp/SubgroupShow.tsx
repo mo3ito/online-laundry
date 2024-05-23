5ea6e15991967cae20383bda1d8387e906331fda
mo3ito <mostafa.tar90@gmail.com>
2024-05-23 10:31:14 +0330

    edit SwiperSlide

--- frontend/components/customerApp/SubgroupShow.tsx
+++ frontend/components/customerApp/SubgroupShow.tsx
@@ -1,43 +1,98 @@
-import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 export default function SubgroupShow() {
   return (
-    <div className='w-full h-44  bg-red-200 px-8 flex items-center justify-center'>
+    <div className="w-full h-44  px-8 flex items-center justify-center bg-white">
       <Swiper
         slidesPerView={5}
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
         className="mySwiper h-40 "
       >
-        <SwiperSlide className='size-40 rounded-lg bg-sky-200 p-3'>
-            <img className='!size-full' src="/images/washing-machine.jpg" alt="kkjj" />
+        <SwiperSlide className="size-40 rounded-lg ">
+          <div className="size-full flex items-center justify-center flex-col ">
+            <figure className="p-2 size-10/12 bg-sky-200 rounded-xl -translate-y-3 cursor-pointer">
+              <img
+                className="size-full rounded-xl"
+                src="/images/washing-machine.jpg"
+                alt="Washing Machine"
+              />
+              <figcaption className="text-center mt-3">زنانه</figcaption>
+            </figure>
+          </div>
         </SwiperSlide>
 
-        <SwiperSlide className='size-40 rounded-lg bg-sky-200 p-3'>
-            <img className='!size-full' src="/images/washing-machine.jpg" alt="kkjj" />
+        <SwiperSlide className="size-40 rounded-lg ">
+          <div className="size-full flex items-center justify-center flex-col ">
+            <figure className="p-2 size-10/12 bg-sky-200 rounded-xl -translate-y-3 cursor-pointer">
+              <img
+                className="size-full rounded-xl"
+                src="/images/washing-machine.jpg"
+                alt="Washing Machine"
+              />
+              <figcaption className="text-center mt-3">زنانه</figcaption>
+            </figure>
+          </div>
         </SwiperSlide>
 
-        <SwiperSlide className='size-40 rounded-lg bg-sky-200 p-3'>
-            <img className='!size-full' src="/images/washing-machine.jpg" alt="kkjj" />
+        <SwiperSlide className="size-40 rounded-lg ">
+          <div className="size-full flex items-center justify-center flex-col ">
+            <figure className="p-2 size-10/12 bg-sky-200 rounded-xl -translate-y-3 cursor-pointer">
+              <img
+                className="size-full rounded-xl"
+                src="/images/washing-machine.jpg"
+                alt="Washing Machine"
+              />
+              <figcaption className="text-center mt-3">زنانه</figcaption>
+            </figure>
+          </div>
         </SwiperSlide>
 
-        <SwiperSlide className='size-40 rounded-lg bg-sky-200 p-3'>
-            <img className='!size-full' src="/images/washing-machine.jpg" alt="kkjj" />
-        </SwiperSlide>
-        
-        <SwiperSlide className='size-40 rounded-lg bg-sky-200 p-3'>
-            <img className='!size-full' src="/images/washing-machine.jpg" alt="kkjj" />
+        <SwiperSlide className="size-40 rounded-lg ">
+          <div className="size-full flex items-center justify-center flex-col ">
+            <figure className="p-2 size-10/12 bg-sky-200 rounded-xl -translate-y-3 cursor-pointer">
+              <img
+                className="size-full rounded-xl"
+                src="/images/washing-machine.jpg"
+                alt="Washing Machine"
+              />
+              <figcaption className="text-center mt-3">زنانه</figcaption>
+            </figure>
+          </div>
         </SwiperSlide>
 
+        <SwiperSlide className="size-40 rounded-lg ">
+          <div className="size-full flex items-center justify-center flex-col ">
+            <figure className="p-2 size-10/12 bg-sky-200 rounded-xl -translate-y-3 cursor-pointer">
+              <img
+                className="size-full rounded-xl"
+                src="/images/washing-machine.jpg"
+                alt="Washing Machine"
+              />
+              <figcaption className="text-center mt-3">زنانه</figcaption>
+            </figure>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className="size-40 rounded-lg ">
+          <div className="size-full flex items-center justify-center flex-col ">
+            <figure className="p-2 size-10/12 bg-sky-200 rounded-xl -translate-y-3 cursor-pointer">
+              <img
+                className="size-full rounded-xl"
+                src="/images/washing-machine.jpg"
+                alt="Washing Machine"
+              />
+              <figcaption className="text-center mt-3">زنانه</figcaption>
+            </figure>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
-  )
+  );
 }
