586d3ddc829335bcd200ac00dfcdcf171140d033
mo3ito <mostafa.tar90@gmail.com>
2024-06-02 13:04:14 +0330

    handle focus and changephone nuber form on enter-phone-number page

--- frontend/app/(customer-app)/application/enter-phone-number/page.tsx
+++ frontend/app/(customer-app)/application/enter-phone-number/page.tsx
@@ -1,6 +1,36 @@
-import React from "react";
+"use client";
+import { useState, useRef, useEffect, FormEvent } from "react";
 
 export default function Page() {
+  const [phoneNumberInput, setPhoneNumberInput] = useState<string>("09");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.setSelectionRange(
+        phoneNumberInput.length,
+        phoneNumberInput.length
+      );
+    }
+  }, []);
+
+  const handleChangePhoneNumber = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newValue = event.target.value;
+    if (!newValue.startsWith("09")) {
+      setPhoneNumberInput("09");
+    } else {
+      setPhoneNumberInput(newValue);
+    }
+  };
+
+  const phoneNumberSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(phoneNumberInput);
+  };
+
   return (
     <div className="w-full h-screen inset-0 bg-slate-100 fixed z-50 flex items-center justify-center">
       <div className="mx-6 w-96 h-max border border-sky-500 rounded-lg -translate-y-20 flex flex-col items-center">
@@ -16,14 +46,17 @@ export default function Page() {
           </div>
         </header>
         <section className="w-full p-2 text-sm sm:text-base">
-          <form className="w-full" >
+          <form onSubmit={phoneNumberSubmitHandler} className="w-full">
             <label className="my-2 inline-block" htmlFor="phone-number-input">
               شماره موبایل خود را وارد کنید
             </label>
             <input
-              className="w-full h-10 bg-transparent border border-sky-500 outline-none rounded-lg px-2"
+              className="w-full h-10 bg-transparent border border-sky-500 outline-none rounded-lg px-2 "
               id="phone-number-input"
+              ref={inputRef}
               type="tel"
+              value={phoneNumberInput}
+              onChange={handleChangePhoneNumber}
             />
             <button className="w-full h-10 bg-sky-200 mt-2 rounded-lg">
               ارسال کد تایید
