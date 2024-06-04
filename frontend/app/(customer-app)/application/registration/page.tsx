629f6c4f2532a260c7e3d2fcba3846a4568d74e2
mo3ito <mostafa.tar90@gmail.com>
2024-06-04 12:54:40 +0330

    add registration page

---
+++ frontend/app/(customer-app)/application/registration/page.tsx
@@ -0,0 +1,39 @@
+import Logo from "@/components/logo/Logo";
+import DefaultButton from "@/components/share/defaultButton";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="w-full h-screen fixed inset-0 bg-slate-100 z-50 flex items-center flex-col justify-center">
+      <section className="max-[380px]:w-full  max-[410px]:px-3 w-96 h-max -translate-y-44">
+        <Logo classNameContainer="bg-transparent " />
+        <form className="w-full h-max border border-sky-500  rounded-lg  flex items-center justify-between flex-col max-[380px]:text-sm text-base ">
+          <h1 className=" bg-sky-200 w-full rounded-t-lg text-center py-2 ">
+            ثبت نام
+          </h1>
+          <div className="px-3  h-full ">
+            <input
+              placeholder="نام"
+              className="h-10 w-full bg-transparent border border-sky-500 rounded-lg outline-sky-600 px-2 my-2"
+              type="text"
+            />
+            <input
+              placeholder="نام خانوادگی"
+              className="h-10 w-full bg-transparent border border-sky-500 rounded-lg outline-sky-600 px-2 my-2"
+              type="text"
+            />
+            <input
+              placeholder="نام کاربری"
+              className="h-10 w-full bg-transparent border border-sky-500 rounded-lg outline-sky-600 px-2 my-2"
+              type="text"
+            />
+            <DefaultButton
+              content="تایید"
+              className="w-full h-10 rounded-lg my-2"
+            />
+          </div>
+        </form>
+      </section>
+    </div>
+  );
+}
