ac8ef0692ed0c9e6fe22b95dca642d9aa405b443
mo3ito <mostafa.tar90@gmail.com>
2024-06-11 13:44:46 +0330

    add result section in order page

--- frontend/app/(customer-app)/application/order/page.tsx
+++ frontend/app/(customer-app)/application/order/page.tsx
@@ -10,14 +10,15 @@ export default function page() {
   return (
     <div
     style={{ height: `calc(100vh - 248px)` }}
-    className="mx-auto w-full sm:w-5/6 md:w-5/6 lg:w-4/6  shadow-xl  overflow-auto border border-sky-500"
+    className="mx-auto w-full sm:w-5/6 md:w-5/6 lg:w-4/6  shadow-xl  overflow-auto border border-sky-500 "
   >
-    <HeaderComponent title="سفارشات شما" as="header" className='sticky top-0 bg-slate-100' />
-    <ul className='w-full h-max p-6'>
+    
+    <HeaderComponent title="سفارشات شما"  as="header" />
+    <ul className='w-full h-max p-6 sm:p-8 '>
     
       {
         orders.map(order=>
-          <li className=' border-2 border-sky-200 bg-sky-100 p-3 rounded-lg mb-4 shadow-xl max-[280px]:text-xs text-sm sm:text-base'>
+          <li key={order.id} className=' border-2 border-sky-200 bg-sky-100 p-3 rounded-lg mb-4 shadow-xl max-[280px]:text-xs text-sm sm:text-base'>
             <article>
           <div className='flex max-[280px]:justify-start justify-between items-center mb-3 gap-x-4'>
           <p>نوع لباس</p>
@@ -44,6 +45,18 @@ export default function page() {
           )
       } 
     </ul>
+
+    <div className=' h-max border border-sky-500 p-2 rounded-lg shadow-xl bg-sky-200 max-[280px]:text-xs text-sm sm:text-base mx-6 sm:mx-8'>
+    <div className='flex max-[280px]:justify-start justify-between  items-center mb-3 gap-x-2  w-full  '>
+          <p>تعداد کل سفارشات </p>
+          <p>5 عدد</p>
+          </div>
+          <div className='flex max-[280px]:justify-start justify-between  items-center  gap-x-2  w-full mb-3 '>
+          <p>مبلغ کل</p>
+          <p>400 هزارتومان</p>
+          </div>
+          <button className='bg-green-500 py-2 w-full rounded-lg text-white'>تایید و ادامه</button>
+    </div>
   </div>
   )
 }
--- frontend/components/customerApp/Header.tsx
+++ frontend/components/customerApp/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
 
   return (
     <>
-      <header className="bg-sky-500 pb-2 max-[280px]:px-3 px-6  sm:px-8 text-white w-full mx-auto shadow-xl sm:w-5/6 md:w-5/6 lg:w-4/6 ">
+      <header className="bg-sky-500 pb-2 max-[280px]:px-3 px-6  sm:px-8 text-white w-full mx-auto  sm:w-5/6 md:w-5/6 lg:w-4/6 ">
         <div className="flex  justify-between pt-5 ">
           <InformationButton onClick={() => console.log("ff")} />
           <ShowName
