c0890fbe53f2a31b28d59ddfe7cab6af8c795f23
mo3ito <mostafa.tar90@gmail.com>
2024-06-12 16:20:46 +0330

    edit in addhandler and minesHandler

--- frontend/app/utils/minesClothingHandler.tsx
+++ frontend/app/utils/minesClothingHandler.tsx
@@ -1,41 +1,3 @@
-// import { OrderCardType } from "@/types/context/OrderCard";
-// import { Dispatch, SetStateAction } from "react";
-// import { toast } from "react-toastify";
-
-// const minesClothingHandler = (
-//   orders: OrderCardType[],
-//   id: string,
-//   setOrders: Dispatch<SetStateAction<OrderCardType[]>>,
-//   typeClothing: string,
-//   cost: number,
-//   count: number
-// ) => {
-//   const isOrder = orders.find((order) => order.id === id);
-
-//   if (isOrder) {
-//     const updatedOrders = orders.map((order) => {
-//       if (order.id === isOrder.id && order.serviceType === typeClothing) {
-//         const updatedCount = order.count - count <= 0 ? 1 : order.count - count;
-//         const updateTotalCost =
-//           order.count - count <= 0 ? cost : order.totalCost - cost;
-//         if (order.count > 1) {
-//           toast.success("سفارش شما با موفقیت کاسته شد");
-//         }
-//         return {
-//           ...order,
-//           count: updatedCount,
-//           totalCost: updateTotalCost,
-//         };
-//       }
-
-//       return order;
-//     });
-//     setOrders(updatedOrders);
-//   }
-// };
-
-// export default minesClothingHandler;
-
 import { OrderCardType } from "@/types/context/OrderCard";
 import { Dispatch, SetStateAction } from "react";
 import { toast } from "react-toastify";
@@ -51,28 +13,27 @@ const minesClothingHandler = (
   const isOrder = orders.find((order) => order.id === id);
 
   if (isOrder) {
-    setOrders((prevOrders) => {
-      const updatedOrders = prevOrders.map((order) => {
-        if (order.id === isOrder.id && order.serviceType === typeClothing) {
-          const updatedCount = order.count - count <= 0 ? 1 : order.count - count;
-          const updateTotalCost =
-            order.count - count <= 0 ? cost : order.totalCost - cost;
-          if (order.count > 1) {
-            toast.success("سفارش شما با موفقیت کاسته شد");
-          }
-          return {
-            ...order,
-            count: updatedCount,
-            totalCost: updateTotalCost,
-          };
+    const updatedOrders = orders.map((order) => {
+      if (order.id === isOrder.id && order.serviceType === typeClothing) {
+        const updatedCount = order.count - count <= 0 ? 1 : order.count - count;
+        const updateTotalCost =
+          order.count - count <= 0 ? cost : order.totalCost - cost;
+        if (order.count > 1) {
+          toast.success("سفارش شما با موفقیت کاسته شد");
         }
+        return {
+          ...order,
+          count: updatedCount,
+          totalCost: updateTotalCost,
+        };
+      }
 
-        return order;
-      });
-      return updatedOrders;
+      return order;
     });
+    setOrders(updatedOrders);
   }
 };
 
 export default minesClothingHandler;
+
 
