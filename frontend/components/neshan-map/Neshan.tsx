0d7629b5f15e9e11cd6d1d961f384e58d4bf24d3
mo3ito <mostafa.tar90@gmail.com>
2024-06-15 18:01:02 +0330

    add getCenter in map

--- frontend/components/neshan-map/Neshan.tsx
+++ frontend/components/neshan-map/Neshan.tsx
@@ -1,58 +1,39 @@
-'use client'
-import { MapComponent, MapTypes } from "@neshan-maps-platform/mapbox-gl-react";
-import "@neshan-maps-platform/mapbox-gl-react/dist/style.css";
-import nmp_mapboxgl from '@neshan-maps-platform/mapbox-gl';
-import { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import { Map } from "@neshan-maps-platform/ol";
-import { fromLonLat, toLonLat } from 'ol/proj';
-import NeshanMap, { NeshanMapRef  } from "@neshan-maps-platform/react-openlayers";
-import LoadingPage from "../Loading/LoadingPage";
-
+'use client';
+import { useState, useEffect, useRef } from 'react';
+import NeshanMap, { NeshanMapRef } from '@neshan-maps-platform/react-openlayers';
+import { toLonLat } from 'ol/proj';
+import LoadingPage from '../Loading/LoadingPage';
 
 type LatLongType = {
-  lat: number,
-  long: number
-}
-
+  lat: number;
+  long: number;
+};
 
 export default function Neshan() {
-
-  const [address, setAddress] = useState();
-  const [latLong , setLatLong]=useState<LatLongType | null>(null)
-  const [clicked , setclicked]=useState(false)
-  const [centerLocation , setCenterLocation]=useState({ latitude: 34.09, longitude: 49.71 })
+  const [latLong, setLatLong] = useState<LatLongType | null>(null);
+  const [centerLocation, setCenterLocation] = useState({ latitude: 34.09, longitude: 49.71 });
   const mapRef = useRef<NeshanMapRef | null>(null);
-  const locationButtonRef = useRef<HTMLButtonElement | null>(null)
 
   useEffect(() => {
-    if (mapRef && mapRef.current && mapRef.current?.map && typeof window !== 'undefined') {
-      mapRef.current.map.switchTrafficLayer(true);
-
-      mapRef.current.map.on('click', function (event) {
-     
-        
-            const coordinates = event.coordinate;
-            const [longitude, latitude] = toLonLat(coordinates);
-            setLatLong({ lat: latitude, long: longitude });
-            setclicked(false)
-          
-          
-        
-
+    if (mapRef.current && mapRef.current?.map && typeof window !== 'undefined') {
+      const map = mapRef.current.map;
+      map.on('moveend', () => {
+        const view = map.getView();
+        const center = view.getCenter();
+        if (center) {
+          const [longitude, latitude] = toLonLat(center);
+          setLatLong({ lat: latitude, long: longitude });
+          setCenterLocation({ latitude, longitude });
+        }
       });
     }
   }, [mapRef]);
-  
-  const loacationHandler = ()=>{
-    setclicked(true)
-  }
 
   const findLocationHandler = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setCenterLocation({latitude:position.coords.latitude , longitude:position.coords.longitude})
+          setCenterLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude });
         },
         (error) => {
           console.error('Error Code = ' + error.code + ' - ' + error.message);
@@ -61,33 +42,35 @@ export default function Neshan() {
     } else {
       console.error('Geolocation is not supported by this browser.');
     }
-  }
-  
+  };
 
-  console.log(latLong);
+  console.log(centerLocation);
   
 
   return (
-    <div className="relative w-full h-[90%]">
-    <NeshanMap
-      ref={mapRef}
-      mapKey="web.1b9b48ae807d4009b26658e973d92ce1"
-      defaultType="neshan"
-      center={centerLocation}
-      style={{ height: "100%", width: "100%" }}
-      zoom={13}
-      traffic={true}
-      poi={true}
-    /> 
-    <button onClick={loacationHandler} ref={locationButtonRef} className="bg-red-300  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-10 absolute ">
-    <img  className="size-full" src="/images/location.png" alt="" />
-    </button>
-
-    <button onClick={findLocationHandler} className="absolute bottom-5">
-    <img  className="size-6" src="/images/location.png" alt="" />
-    </button>
+    <div className="relative w-full h-[93%]">
+      <NeshanMap
+        ref={mapRef}
+        mapKey="web.1b9b48ae807d4009b26658e973d92ce1"
+        defaultType="neshan"
+        center={centerLocation}
+        style={{ height: '100%', width: '100%' }}
+        zoom={13}
+        traffic={true}
+        poi={true}
+      />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+        <img className="h-10 w-10" src="/images/location.png" alt="Location Marker" />
+      </div>
+      <button onClick={findLocationHandler} className="absolute bottom-5">
+        <img className="size-6" src="/images/location.png" alt="" />
+      </button>
+      {latLong && (
+        <div className="absolute top-0 left-0 m-4 p-2 bg-white rounded shadow">
+          <p>Lat: {latLong.lat.toFixed(6)}</p>
+          <p>Long: {latLong.long.toFixed(6)}</p>
+        </div>
+      )}
     </div>
   );
 }
-
-
