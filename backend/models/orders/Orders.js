3969718174a638cc4a4bbbd90c3577069f568384
mo3ito <mostafa.tar90@gmail.com>
2024-06-16 12:23:13 +0330

    add orders schema

---
+++ backend/models/orders/Orders.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const OrdersSchema = new Schema({
+  name: {
+    type: String,
+  },
+  last_name: {
+    type: String,
+  },
+  orders: {
+    type: [],
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  latitude: {
+    type: String,
+    required: true,
+  },
+  longitude: {
+    type: String,
+    required: true,
+  },
+});
+
+const Orders = mongoose.model("orders", OrdersSchema);
+
+module.exports = Orders;
