d67773321bd932855ee17cfc8fce3c941c75e85b
mo3ito <mostafa.tar90@gmail.com>
2024-06-22 12:17:07 +0330

    add edit information page

--- frontend/components/customerApp/share/buttons/InformationButton.tsx
+++ frontend/components/customerApp/share/buttons/InformationButton.tsx
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-
-type InformationButtonProps = {
-    onClick: () => void;
-  };
-
-export default function InformationButton({onClick}:InformationButtonProps) {
+export default function InformationButton() {
   return (
-    <button className='size-max' onClick={onClick}>
-    <svg
-    className="feather feather-edit stroke-2 size-5 sm:size-7"
-    fill="none"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    viewBox="0 0 24 24"
-    width="24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-  </svg>
-  </button>
-  )
+    <Link href={"/application/edit-information"} className="size-max">
+      <svg
+        className="feather feather-edit stroke-2 size-5 sm:size-7"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+      </svg>
+    </Link>
+  );
 }
