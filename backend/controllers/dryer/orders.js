9cd45b93a5963d516500611e103d00cda63efd0e
mo3ito <mostafa.tar90@gmail.com>
2024-07-04 12:20:09 +0330

    edit on doneOrder api

--- backend/controllers/dryer/orders.js
+++ backend/controllers/dryer/orders.js
@@ -12,9 +12,10 @@ ordersForDryer = async (req, res) => {
       });
     }
 
-    const orders = await OrdersModel.find({});
+    const orders = await OrdersModel.find({is_done_all_order : false});
+
     const filteredCollections = await orders.filter((item) =>
-      item.orders.every((item) => item.situation === "تحویل گرفته شده")
+      item.orders.every((item) => item.situation === "تحویل گرفته شده" ) 
     );
 
     return res.status(200).json(filteredCollections);
@@ -26,16 +27,75 @@ ordersForDryer = async (req, res) => {
   }
 };
 
+// const doneOrder = async (req, res) => {
+//   const dryerId = req.headers.authorization;
+//   const { customerId, orders_id } = req.body;
 
+//   try {
+//     const Dryer = await DryerModel.findById(dryerId);
+//     const customerOrdersList = await OrdersModel.find({
+//       customer_id: customerId,
+//     });
+
+//     if (!Dryer) {
+//       return res.status(400).json({
+//         message: "خشکشویی با این آیدی وجود ندارد",
+//       });
+//     }
+
+//     if (!customerOrdersList.length) {
+//       return res.status(400).json({
+//         message: "مشتری با این آیدی وجود ندارد",
+//       });
+//     }
+
+//     let orderFound = false;
+
+//     for (const customerOrders of customerOrdersList) {
+//       customerOrders.orders = customerOrders.orders.map((order) => {
+//         if (order.orders_id === orders_id) {
+//           orderFound = true;
+//           return {
+//             ...order,
+//             is_done_order: true,
+//           };
+//         }
+//         return order;
+//       });
+
+//       const allDone = customerOrders.orders.every(
+//         (order) => order.is_done_order
+//       );
+//       customerOrders.is_done_all_order = allDone;
+
+//       await customerOrders.save();
+//     }
+
+//     if (!orderFound) {
+//       return res.status(400).json({
+//         message: "سفارشی با این آیدی وجود ندارد",
+//       });
+//     }
+
+//     return res.status(200).json({
+//       message: "سفارش با موفقیت به‌روز شد",
+//     });
+//   } catch (error) {
+//     console.error("error:", error.message);
+//     return res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// };
 
 const doneOrder = async (req, res) => {
   const dryerId = req.headers.authorization;
-  const { customerId, orders_id } = req.body;
+  const { customer_id, _id } = req.body;
 
   try {
     const Dryer = await DryerModel.findById(dryerId);
     const customerOrdersList = await OrdersModel.find({
-      customer_id: customerId,
+      customer_id: customer_id,
     });
 
     if (!Dryer) {
@@ -50,36 +110,13 @@ const doneOrder = async (req, res) => {
       });
     }
 
-    let orderFound = false;
+    const order = await OrdersModel.findOne({ customer_id, _id });
 
-    for (const customerOrders of customerOrdersList) {
-      customerOrders.orders = customerOrders.orders.map((order) => {
-        if (order.orders_id === orders_id) {
-          orderFound = true;
-          return {
-            ...order,
-            is_done_order: true,
-          };
-        }
-        return order;
-      });
-
-      const allDone = customerOrders.orders.every(
-        (order) => order.is_done_order
-      );
-      customerOrders.is_done_all_order = allDone;
-
-      await customerOrders.save();
-    }
-
-    if (!orderFound) {
-      return res.status(400).json({
-        message: "سفارشی با این آیدی وجود ندارد",
-      });
-    }
+    order.is_done_all_order = true;
+    await order.save();
 
     return res.status(200).json({
-      message: "سفارش با موفقیت به‌روز شد",
+      message: "انجام سرویس با موفقیت ثبت شد",
     });
   } catch (error) {
     console.error("error:", error.message);
