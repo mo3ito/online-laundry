e8aaa93c3672e3fe7aec36e6fece57fe7f7f837a
mo3ito <mostafa.tar90@gmail.com>
2024-07-10 15:43:10 +0330

    add verify driver api

--- backend/controllers/admin/control.js
+++ backend/controllers/admin/control.js
@@ -23,4 +23,50 @@ const getAllDriver = async (req, res) => {
   }
 };
 
-module.exports = { getAllDriver };
+const verifyDriver = async (req, res) => {
+  const adminId = req.headers.authorization;
+  const { driver_id } = req.body;
+
+  if (!adminId) {
+    return res.status(400).json({
+      message: "آیدی ادمین وجود ندارد",
+    });
+  }
+
+  if (!driver_id) {
+    return res.status(400).json({
+      message: "آیدی راننده وجود ندارد",
+    });
+  }
+
+  try {
+    const admin = await AdminModel.findById(adminId);
+    if (!admin) {
+      return res.status(404).json({
+        message: "ادمینی با این آیدی وجود ندارد",
+      });
+    }
+
+    const driver = await DriverModel.findById(driver_id);
+    if (!driver) {
+      return res.status(404).json({
+        message: "راننده‌ای با این آیدی وجود ندارد",
+      });
+    }
+
+    driver.is_register_by_admin = true;
+    await driver.save();
+
+    const alldriver = await DriverModel.find({});
+
+    return res.status(200).json(alldriver);
+  } catch (error) {
+    console.error("error:", error.message);
+    return res.status(500).json({
+      message: "خطایی رخ داد",
+    });
+  }
+};
+
+
+module.exports = { getAllDriver  , verifyDriver};
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,10 +5,11 @@ const {
   adminRegister,
   adminLogin,
 } = require("../controllers/admin/registration");
-const { getAllDriver } = require("../controllers/admin/control");
+const { getAllDriver, verifyDriver } = require("../controllers/admin/control");
 
 router.post("/admin/register", adminRegister);
 router.post("/admin/login", adminLogin);
 router.get("/admin/get-all-drivers", getAllDriver);
+router.put("/admin/verify-driver", verifyDriver);
 
 module.exports = router;
