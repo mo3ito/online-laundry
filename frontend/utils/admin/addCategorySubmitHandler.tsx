4751c214002408a12cc747a377d37fea3569fed0
mo3ito <mostafa.tar90@gmail.com>
2024-07-11 10:23:43 +0330

    edit on addCategorySubmitHandler

--- frontend/utils/admin/addCategorySubmitHandler.tsx
+++ frontend/utils/admin/addCategorySubmitHandler.tsx
@@ -13,6 +13,7 @@ const addCategorySubmitHandler = async (
   setClothingCategoryEnglish: Dispatch<SetStateAction<string>>
 ) => {
   event.preventDefault();
+
   const body = {
     name: clothingCategory,
     english_name: clothingCategoryEnglish,
@@ -20,29 +21,24 @@ const addCategorySubmitHandler = async (
 
   try {
     setIsLoadingForSendCategory(true);
+
     const response = await sendData(CLOTHING_CATEGORY_ADD_CATEGORY, body, _id);
     if (response.status === 200) {
-      setIsLoadingForSendCategory(false);
       toast.success("دسته‌بندی با موفقیت اضافه شد");
       setClothingCategory("");
       setClothingCategoryEnglish("");
     } else {
-      setIsLoadingForSendCategory(false);
+      toast.error("متاسفانه خطایی رخ داده است. لطفاً دوباره تلاش کنید.");
     }
   } catch (error: any) {
     console.error("خطا در ارتباط با سرور:", error);
-
-    if (error.response && error.response.status === 400) {
-      setIsLoadingForSendCategory(false);
-      const errorMessage: string =
-        error.response.data?.message || "خطایی رخ داده است.";
-      toast.error(errorMessage);
-    } else {
-      setIsLoadingForSendCategory(false);
-      console.log("خطا:", error);
-      toast.error("متاسفانه خطایی رخ داده است. لطفاً دوباره تلاش کنید.");
-    }
+    const errorMessage =
+      error.response?.data?.message || "متاسفانه خطایی رخ داده است. لطفاً دوباره تلاش کنید.";
+    toast.error(errorMessage);
+  } finally {
+    setIsLoadingForSendCategory(false);
   }
 };
 
 export default addCategorySubmitHandler;
+
