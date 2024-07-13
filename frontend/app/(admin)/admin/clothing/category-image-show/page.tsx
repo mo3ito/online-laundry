2a5ebd682d403adde44de9865a7cfb9ea97d60dc
mo3ito <mostafa.tar90@gmail.com>
2024-07-13 15:48:19 +0330

    add admin get all category and delete category images endpoints

--- frontend/app/(admin)/admin/clothing/category-image-show/page.tsx
+++ frontend/app/(admin)/admin/clothing/category-image-show/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ShowImagesForAdmin from "@/components/admin/ShowImagesForAdmin";
+import {
+  ADMIN_GET_ALL_CATEGORY_IMAGES,
+  ADMIN_DELETE_CATEGORY_IMAGES,
+} from "@/routeApi/endpoints";
 
 export default function page() {
   return (
     <ShowImagesForAdmin
-      deleteImageApi="http://localhost:4000/admin/delete-category-image"
-      getAllImageApi="http://localhost:4000/admin/get-all-category-images"
+      deleteImageApi={ADMIN_DELETE_CATEGORY_IMAGES}
+      getAllImageApi={ADMIN_GET_ALL_CATEGORY_IMAGES}
       headersPage="عکس‌های دسته‌بندی"
     />
   );
