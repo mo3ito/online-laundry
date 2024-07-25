daf09e4dd363a279307d17439d2324793be91de5
mo3ito <mostafa.tar90@gmail.com>
2024-07-25 11:43:30 +0330

    add download application link on qr code

--- frontend/app/(admin)/admin/qr-code/page.tsx
+++ frontend/app/(admin)/admin/qr-code/page.tsx
@@ -1,14 +1,24 @@
-import QRCodeLinkShower from '@/components/QRCodeLinkShower'
-import ShowHeaderTitle from '@/components/customerSite/ShowHeaderTitle'
-import ShowHeaderTitleFixed from '@/components/customerSite/ShowheaderTitleFixed'
-import React from 'react'
+import QRCodeLinkShower from "@/components/QRCodeLinkShower";
+import ShowHeaderTitle from "@/components/customerSite/ShowHeaderTitle";
+import ShowHeaderTitleFixed from "@/components/customerSite/ShowheaderTitleFixed";
+import React from "react";
 
 export default function page() {
   return (
-    <main className=' mt-36 md:mt-64 pb-10'>
-    <ShowHeaderTitleFixed content='QR code'/>
-    <QRCodeLinkShower QRCodePath={`${process.env.NEXT_PUBLIC_BASE_URL}`} content='لینک سایت' />
-    <QRCodeLinkShower QRCodePath={`${process.env.NEXT_PUBLIC_BASE_URL}/application`} content='لینک اپلیکیشن' />
+    <main className=" mt-36 md:mt-64 pb-10">
+      <ShowHeaderTitleFixed content="QR code" />
+      <QRCodeLinkShower
+        QRCodePath={`${process.env.NEXT_PUBLIC_BASE_URL}`}
+        content="لینک سایت"
+      />
+      <QRCodeLinkShower
+        QRCodePath={`${process.env.NEXT_PUBLIC_BASE_URL}/application`}
+        content="لینک اپلیکیشن"
+      />
+      <QRCodeLinkShower
+        QRCodePath={"https://mo3ito-pak.ir"}
+        content="دانلود اپلیکیشن"
+      />
     </main>
-  )
+  );
 }
