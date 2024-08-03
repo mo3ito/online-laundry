40903f6c139ef8cee7a5158e7c40a98041a4d4cb
mo3ito <mostafa.tar90@gmail.com>
2024-08-03 16:55:06 +0330

    edit on screen header site

--- frontend/components/customerSite/header/ScreenHeader.tsx
+++ frontend/components/customerSite/header/ScreenHeader.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function ScreenHeader() {
   return (
-    <header className=" h-max w-full  mx-auto bg-gray-200  hidden sm:block fixed inset-x-0 top-0">
+    <header className=" h-max w-full  mx-auto bg-gray-200  hidden sm:block fixed inset-x-0 top-0 z-50">
       <div className="container mx-auto px-6 py-3 ">
         <div className=" w-full flex items-center justify-between mb-3">
           <div className="flex items-center gap-x-2 ">
@@ -31,7 +31,7 @@ export default function ScreenHeader() {
         </div>
         <nav>
           <ul className="bg-gray-500 w-full h-16 flex items-center justify-between rounded-md sm:text-sm lg:text-base 2xl:text-lg">
-            <li className=" w-1/4 h-full ">
+            <li className=" w-1/3 h-full ">
               <Link
                 className="w-full h-full hover:bg-sky-500 text-white  flex items-center justify-center"
                 href="/"
@@ -39,10 +39,7 @@ export default function ScreenHeader() {
                 خانه
               </Link>
             </li>
-            <li className=" hover:bg-sky-500 w-1/4 h-full flex items-center justify-center cursor-pointer text-white ">
-              خدمات خشکشویی
-            </li>
-            <li className=" w-1/4 h-full ">
+            <li className=" w-1/3 h-full ">
               <Link
                 className="w-full h-full hover:bg-sky-500 text-white  flex items-center justify-center"
                 href="/price-list"
@@ -50,7 +47,7 @@ export default function ScreenHeader() {
                 لیست قیمت‌ها
               </Link>
             </li>
-            <li className=" w-1/4 h-full ">
+            <li className=" w-1/3 h-full ">
               <Link
                 className="w-full h-full hover:bg-sky-500 text-white  flex items-center justify-center"
                 href="/contact-us"
